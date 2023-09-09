f12b80f2e609aed53d3177b7f6769e5a1d9b2b38
San-Ba-Wi <144503132+San-Ba-Wi@users.noreply.github.com>
2023-09-09 11:50:52 +0900

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from variable_config import ANSWERS
 
 audio_file = open('school-music.mp3', 'rb')
 audio_bytes = audio_file.read()
@@ -14,16 +13,13 @@ answer = st.radio(
 )
 if answer == '대구고등학교 교가':
     st.write(':blue[쩡답]입니다!')
-    ANSWERS[0] = 1
     st.write(':green[왼쪽의 페이지에서 다음 문제로 넘어가주세요!]')
     st.image('correct!.webp')
 elif answer == '정답을 선택해주세요':
     st.write()
-    ANSWERS[0] = 0
     st.image('thinking.jpg')
 else:
     st.write(':red[하지만 그것은 틀렸습니다!]')
-    ANSWERS[0] = 0
     st.image('tryagain.jpg')
 
 import os
